6decd181289d878239bf98fd10ec5c69b1d83c96
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-07 18:31:16 -0300

    blueprint de diretor no init.py

--- ExpressMovies/app/__init__.py
+++ ExpressMovies/app/__init__.py
@@ -11,6 +11,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     
     from .routes.filmes_routes import filmes_bp
+    from .routes.diretor_routes import diretor_blueprint
     app.register_blueprint(filmes_bp)
+    app.register_blueprint(diretor_blueprint)
 
     return app
