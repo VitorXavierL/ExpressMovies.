acb0fc54b9b477eb4feb5e6284545de4cfd1b562
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-07 15:47:45 -0300

    adicionando o model de diretor

---
+++ ExpressMovies/app/models/diretor.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Diretor(db.Model):
+    """
+    Modelo Diretor : Representa a tabela diretor no banco de dados
+    """
+    __tablename__ = 'diretor'
+
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    nome = db.Column(db.String(100),nullable=False)
+    data_nasc = db.Column(db.Date)
+    filmes_id = db.Column(db.ForeignKey('filme.id'))
+
+    def dici(self):
+        return {'id':self.id,'nome':self.nome,'data_nascimento':self.data_nasc,'filmes':list(self.filmes_id)}
