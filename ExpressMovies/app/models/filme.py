0507b67b8a7534ad13f41302e04375c21347f002
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-07 16:21:00 -0300

    tirando o s do fim do nome da tabela

--- ExpressMovies/app/models/filme.py
+++ ExpressMovies/app/models/filme.py
@@ -4,7 +4,7 @@ class Filme(db.Model):
     """
     Modelo Filme - Representa a tabela 'filme' no banco de dados.
     """
-    __tablename__ = 'filmes'
+    __tablename__ = 'filme'
 
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(100), nullable=False)
