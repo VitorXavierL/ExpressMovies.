23a70c011241b3c1edfb86cdd0c891bb5c6cf4bc
Gabriel Carvalho Vieira <2401423@SALAS.AULAS>
2025-10-08 11:24:50 -0300

    colocando a variavel api dentro do init.py

--- ExpressMovies/app/__init__.py
+++ ExpressMovies/app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from swagger.swagger_config import configure_swagger
+from flask_restx import Api
+
+
+api = Api (
+    version='1.0',
+    title='Api ExpressMovies',
+    description='Documentação para filmes, diretor',
+    doc='/docs',
+    mask_swagger = False,
+    prefix='/api'
+)
 
 db = SQLAlchemy()
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -14,5 +27,8 @@ def create_app(config_class=Config):
     from .routes.diretor_routes import diretor_blueprint
     app.register_blueprint(filmes_bp)
     app.register_blueprint(diretor_blueprint)
+
+    configure_swagger(api,app)
+
 
     return app
