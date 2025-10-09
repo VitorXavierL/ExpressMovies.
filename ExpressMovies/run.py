79d20f8442b81123106a918ddd21b563623d52c2
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-08 21:20:15 -0300

    adicionando o appcontext dentro do if

--- ExpressMovies/run.py
+++ ExpressMovies/run.py
@@ -1,13 +1,20 @@
 from app import create_app, db
+from app.models.diretor import Diretor
+from app.models.filme import Filme
 
 app = create_app()
 
 @app.cli.command("create-db")
 def create_db_command():
-    
-    db.create_all()
-    print("Banco de dados criado com sucesso!")
+    with app.app_context():
+
+        db.create_all()
+        print("Banco de dados criado com sucesso!")
 
 
 if __name__ == '__main__':
+   
+   with app.app_context():
+       db.create_all()
+       
    app.run(host='0.0.0.0',port=5000,debug=True)
