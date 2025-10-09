bc35e64ed292592e061c6a565946ca7af81b830e
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-08 21:18:18 -0300

    retirada da variavel app e da funçao init_app

--- ExpressMovies/swagger/swagger_config.py
+++ ExpressMovies/swagger/swagger_config.py
@@ -2,8 +2,7 @@ from .namespaces.filme_namespace import filme_ns
 from .namespaces.diretor_namespaces import diretor_ns
 
 
-def configure_swagger(api,app):
-    api.init_app(app)
+def configure_swagger(api):
     api.add_namespace(filme_ns,path='/filmes')
     api.add_namespace(diretor_ns,path='/diretores')
     api.mask_swagger = False
