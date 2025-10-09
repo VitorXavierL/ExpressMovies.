57cbc1e0dd254df1eaee91adb516fe58191ffe44
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-10-08 21:16:28 -0300

    mudança no nome de data_nasc

--- ExpressMovies/app/models/diretor.py
+++ ExpressMovies/app/models/diretor.py
@@ -8,8 +8,8 @@ class Diretor(db.Model):
 
     id = db.Column(db.Integer,primary_key=True,nullable=False)
     nome = db.Column(db.String(100),nullable=False)
-    data_nasc = db.Column(db.Date)
+    data_nascimento = db.Column(db.Date)
     filmes_id = db.Column(db.ForeignKey('filme.id'))
 
     def dici(self):
-        return {'id':self.id,'nome':self.nome,'data_nascimento':self.data_nasc,'filmes_id':self.filmes_id}
+        return {'id':self.id,'nome':self.nome,'data_nascimento':self.data_nascimento,'filmes_id':self.filmes_id}
